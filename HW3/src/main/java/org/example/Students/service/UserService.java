eb6c90454ed0bf5fb2a58f592128feb404d53275
Kellamon <Badzed@list.ru>
2023-05-04 10:04:21 +0300

    Добавлено ДЗ 6

--- HW3/src/main/java/org/example/Students/service/UserService.java
+++ HW3/src/main/java/org/example/Students/service/UserService.java
@@ -1,9 +1,11 @@
 package org.example.Students.service;
 
+import org.example.Students.data.User;
+
 import java.time.LocalDate;
 import java.util.List;
 
-public interface UserService<T> {
+public interface UserService<T extends User> {
     List<T> getAll();
     void create(String firstName, String secondName, String patronymic, LocalDate dateOfBirth);
 }
